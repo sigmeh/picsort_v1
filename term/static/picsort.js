948853140f274568bc6f5b6afa08a828815d8bd0
Mark Harvey <markedwinharvey@gmail.com>
2017-06-06 20:37:04 +0200

    modularize ajax

--- term/static/picsort.js
+++ term/static/picsort.js
@@ -5,31 +5,53 @@ $(document).on('click','.x',function(){
 	$(this).parent().remove();
 });
 
-function populate_dir_list(){
-	$('#dir_list').html('');
+function submit_picsort( submission ){
 	$.ajax({
-		method:	'post',
-		url:	'picsort.py',
-		data:	{'package':'populate_dir_list'},
-		success:	function(dir_list){
-			dir_list = JSON.parse(dir_list);
-			for (i=0; i < dir_list.length; i++){
-				if ( $.inArray(dir_list[i],removed_dirs) != -1){
-					//ignore previously-removed directories
-					continue;
-				}
-				
-				$('#dir_list').append(
-					'<div class="dir_outer">'+
-						'<img class="x border" src="media/x.png" />&nbsp'+
-						'<span class="dir">'+dir_list[i]+'</span>'+
-					'</div>'
-				);
-			}
+		method 	: 'post',
+		url		: 'picsort.py',
+		data	: { 'package' : JSON.stringify(submission) }, 
+		success : function( result ){
+			
+			result = JSON.parse(result);		
+			f = result.callback_function;
+			a = result.args;
+			 
+			eval( f+'('+JSON.stringify(a)+')' );
 		}
 	});
 }
 
+function populate_dir_list( dir_list ){
+	//  Function 1: dir_list == null; get directory list from current_dir (in current_session)
+	//  Function 2: dir_list returned on callback function; clear #dir_list and append new directory content
+	
+	if ( dir_list ){		//  Put dir_list in #dir_list
+		$('#dir_list').html('');	
+		$(dir_list).each(function(){
+			$('#dir_list').append(
+				'<div class="dir_outer">'+
+					'<img class="x border" src="media/x.png" />&nbsp'+
+					'<span class="dir">'+this.substring( 0, this.length )+
+					'</span>'+
+				'</div>'
+			);
+		});		
+	}
+	else{
+		submit_picsort({'instructions' : 'populate_dir_list'});
+	}
+}
+
+function get_pic_list( pic_list ){
+	
+	if ( pic_list){
+		
+	}
+	else{	
+		submit_picsort({'instructions' : 'get_pic_list'});
+	}
+}
+
 $(document).ready(function(){
-	populate_dir_list();
+	populate_dir_list( null );
 });
